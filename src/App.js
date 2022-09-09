548421247cd792eef04a889d3071017ef465d3d5
Sashankr <sashankr97@gmail.com>
2022-09-09 20:03:43 +0530

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
 import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Notes App</Text>
+      <Header />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
   },
 });
 
---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    backgroundColor: '#ffba08',
+  },
+  heading: {
+    textAlign: 'center',
+    fontSize: 24,
+    paddingVertical: 10,
+  },
+});
+
+export default styles;
